de04267cf9527d21bcc80080c02a959324f2a6eb
Subrat <subrat.1108@gmail.com>
2017-10-22 14:37:20 +0530

    profile page optimization

--- public/javascripts/fotomartApp.js
+++ public/javascripts/fotomartApp.js
@@ -222,6 +222,19 @@ app.controller("competitionController", function($scope){
 
 app.controller("profileController", function($scope,settingService,$rootScope,$http,Upload){
 
+  
+  $scope.openNav = function(){
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  $scope.closeNav = function(){
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+      document.body.style.backgroundColor = "white";
+  }
+
 
 
   $scope.subView = "userPortfolio.html";
